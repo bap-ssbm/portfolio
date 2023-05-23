11cbb787e27950afe8f20385d41da43758ef5eda
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-05-23 14:48:00 +0900

    Add files via upload

--- app/portfolio/seta-tei/page.tsx
+++ app/portfolio/seta-tei/page.tsx
@@ -1,16 +1,13 @@
 import { FC } from 'react'
 import AboutTemplate from '../components/AboutTemplate'
 
-interface pageProps {
-
-}
-
+interface pageProps {}
 const page: FC<pageProps> = ({ }) => {
     return (
         <AboutTemplate title='Restaurant Page / Hayama Seta-tei'
             src='/images/portfolio/personal/new-setatei.jpg'
-            github='https://github.com/bap-ssbm/seta-react-draft'
-            link='https://seta-react-draft.vercel.app/'>
+            github='https://github.com/bap-ssbm/seta-tei'
+            link='https://seta-tei.vercel.app/'>
             <div className='text-center'>
                 <h3 className='text-[16px] font-Oswald underline mb-4'>
                     Tech Stack
@@ -24,24 +21,14 @@ const page: FC<pageProps> = ({ }) => {
                 About
             </h2>
             <p className='mb-5'>
-                This is a draft of the website I'm currently remaking for my auntie's restaurant.
+                This is an official website I made for the French Restaurant called Seta-tei
             </p>
             <p>
-                Her current website is made on wordpress, but she mentioned to me that she has been wanting to remake it and make it look better,
+                The Restaurant's previous website was made on wordpress, but the owner mentioned to me that she has been wanting to remake it and make it look better,
                 so I decided to lend a helping hand by recreating the website from scratch with ReactJs.
                 I was initially planning to make the website with NextJS; however, upon deployment with i18n functionality (internationalization-framework), there were alot of complicated issues.
                 Therefore, I decided to try i18n with React instead, and it went alot smoother.
             </p>
-            <p>
-                Since this is just a draft, this is not the final appearance. We are planning to retake the pictures since these pictures are from 2015.
-                Making the Menu page, and the other pages look better in the future is a priority as well.
-                I also want to try my hand in creating a backend for making reservations as well as creating a mobile app with react native so that it's easier 
-                to keep track of reservations. 
-            </p>
-
-
-
-
         </AboutTemplate>
     )
 }
